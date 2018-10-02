922ad85eb3be84cf2569827e72d34cf66a0b3022
Jaakko Wallo <jaakko.wallo@solita.fi>
2018-10-02 18:08:05 +0300

    Loput tehtävät

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -71,54 +71,87 @@
   (map :title books))
 
 (defn monotonic? [a-seq]
-  :-)
+  (or (apply <= a-seq)
+      (apply >= a-seq)))
 
 (defn stars [n]
-  :-)
+  (apply str (repeat n "*")))
 
 (defn toggle [a-set elem]
-  :-)
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)))
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  (< (count (set a-seq)) (count a-seq)))
 
 (defn old-book->new-book [book]
-  :-)
+  (assoc book :authors (set (:authors book))))
 
 (defn has-author? [book author]
-  :-)
+  (contains? (:authors book) author))
 
 (defn authors [books]
-  :-)
+  (let [auth (fn [book] (:authors book))]
+    (apply clojure.set/union (map auth books))))
 
 (defn all-author-names [books]
-  :-)
+  (set (map :name (authors books))))
 
 (defn author->string [author]
-  :-)
+  (let [name (:name author)
+        birth-year (:birth-year author)
+        death-year (:death-year author)
+        by-exists (not (or (= "" birth-year) (nil? birth-year)))]
+    (str name
+         (if by-exists
+           (str " ("
+                birth-year
+                " - "
+                death-year
+                ")")))))
 
 (defn authors->string [authors]
-  :-)
+  (let [commafy (fn [author]
+                  (str (author->string author) ", "))]
+    (apply str (concat (map commafy (butlast authors))
+                       (author->string (last authors))))))
 
 (defn book->string [book]
-  :-)
+  (if (nil? book)
+    ""
+    (str (:title book)
+         ", written by "
+         (authors->string (:authors book)))))
 
 (defn books->string [books]
-  :-)
+  (let [c (count books)
+        count-str (cond
+                    (= 0 c) "No books"
+                    (= 1 c) "1 book. "
+                    :else (str c " books. "))
+        stopfy (fn [book]
+                 (str (book->string book) ". "))]
+    (str count-str
+         (apply str
+                (concat (map stopfy (butlast books))
+                        (book->string (last books))))
+         ".")))
 
 (defn books-by-author [author books]
-  :-)
+  (filter (fn [b] (has-author? b author)) books))
 
 (defn author-by-name [name authors]
-  :-)
+  (first (filter (fn [a] (= name (:name a)))
+                 authors)))
 
 (defn living-authors [authors]
-  :-)
+  (filter alive? authors))
 
 (defn has-a-living-author? [book]
-  :-)
+  (not (empty? (living-authors (:authors book)))))
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter has-a-living-author? books))
 
 ; %________%
